691dcae2fc9ab4d213d38b19725ebf13bfe623f9
nestorrig <neriga10@hotmail.com>
2023-12-03 15:22:08 -0600

    feat: start project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+      Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
