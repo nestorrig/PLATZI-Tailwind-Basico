dd77c22bb736b86325751230181cba33a381ce42
nestorrig <neriga10@hotmail.com>
2023-12-05 00:26:45 -0600

    Clase 12: Tailwind como API para la creación de un Design System

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
+      <h1 className="text-3xl font-bold underline text-secondary bg-primary">
+      Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline text-terciary bg-secondary">
+      Hello world!
+      </h1>
+      <h1 className="text-4xl font-bold font-Montserrat underline text-primary bg-terciary">
       Hello world!
       </h1>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,38 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'sanFrancisco': "url('./src/assets/imgs/sanFrancisco.jpg')",
+        'sanFranciscoDesktop': "url('./src/assets/imgs/sanFranciscoDesktop.jpg')",
+        'yosemite': "url('./src/assets/imgs/yosemite.jpg')",
+        'LA': "url('./src/assets/imgs/LA.jpg')",
+        'seattle': "url('./src/assets/imgs/seattle.jpg')",
+        'new_york': "url('./src/assets/imgs/new_york.jpg')",
+        'norway': "url('./src/assets/imgs/norway.jpg')",
+        'sydney': "url('./src/assets/imgs/sydney.jpg')",
+        'miami': "url('./src/assets/imgs/miami.jpg')",
+        'switzerland': "url('./src/assets/imgs/switzerland.jpg')",
+        'bali': "url('./src/assets/imgs/bali.jpg')",
+        'chicago': "url('./src/assets/imgs/chicago.jpg')",
+        'europe': "url('./src/assets/imgs/europe.jpg')",
+        'iceland': "url('./src/assets/imgs/iceland.jpg')",
+      },
+      backgroundColor: {
+        ...theme => theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9',
+      },
+      textColor: {
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'terciary': '#61AEC9',
+      },
+      fontFamily: { 
+        Montserrat: ["Monserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
